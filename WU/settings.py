557c1f27558405127f09f83ad9e3e0aa08b910ae
jaed <edgar.alvarez@blockbliss.com>
2018-11-26 13:24:05 -0600

    add database

--- WU/settings.py
+++ WU/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'WU.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgresqldatabase36512',
+        'USER': 'postgresqldbuser@wut-postgresqldbserver',
+        'PASSWORD': 'Fundary2018',
+        'HOST': 'wut-postgresqldbserver.postgres.database.azure.com',
+        'PORT': '5432',
     }
 }
 
